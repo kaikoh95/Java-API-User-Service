f132cb8bceea3cf44228f9412639f53dde829913
Kai Koh <kaikoh95@gmail.com>
2021-05-27 08:47:33 +1200

    Develop: Main controller logic.

---
+++ src/main/java/com/middleware/userservice/controllers/UserController.java
@@ -0,0 +1,88 @@
+package com.middleware.userservice.controllers;
+
+import com.middleware.userservice.exceptions.UserNotCreatedException;
+import com.middleware.userservice.exceptions.UserNotDeletedException;
+import com.middleware.userservice.exceptions.UserNotFoundException;
+import com.middleware.userservice.exceptions.UserNotUpdatedException;
+import com.middleware.userservice.exceptions.UserNotValidException;
+import com.middleware.userservice.models.User;
+import com.middleware.userservice.repository.UserRepository;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for User REST services
+ */
+@RestController
+class UserController {
+
+  private final UserRepository repository;
+
+  UserController(UserRepository repository) {
+    this.repository = repository;
+  }
+
+  @PostMapping("/user")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  String createUser(@RequestBody User newUser) {
+    if (!newUser.containsRequired()) {
+      throw new UserNotValidException(newUser.getEmail());
+    }
+    try {
+      System.out.println(newUser);
+      repository.save(newUser);
+      return "The user was created successfully";
+    } catch (Exception e) {
+      throw new UserNotCreatedException(newUser.getEmail());
+    }
+  }
+
+  @GetMapping("/user/{email}")
+  String getUser(@PathVariable String email) {
+    
+    User user = repository.findById(email)
+      .orElseThrow(() -> new UserNotFoundException(email));
+    return user.toString();
+  }
+
+  @PutMapping("/user/{email}")
+  String updateAUser(@RequestBody User newUser, @PathVariable String email) {
+    if (!newUser.containsRequired()) {
+      throw new UserNotValidException(email);
+    }
+    String response = "";
+    response = repository.findById(email)
+      .map(user -> {
+        if (user.validateUser(newUser)) {
+          user.updateUser(newUser);
+          user.setEmail(email);
+          try {
+            repository.save(user);
+          } catch (Exception e) {
+            throw new UserNotUpdatedException(user.getEmail());
+          }
+          return "The user was updated successfully";
+        }
+        throw new UserNotValidException(email);
+      });
+      return response;
+  }
+
+  @DeleteMapping("/user/{email}")
+  String deleteUser(@PathVariable String email) {
+    try {
+      repository.deleteById(email);
+    } catch (Exception e) {
+      throw new UserNotDeletedException(email);
+    }
+    return "The user was deleted successfully";
+  }
+}
